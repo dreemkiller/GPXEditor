5b91e6dd21c4791b7589f0a1a3ed4eb683afac06
Derek D. Miller <dreemkiller@gmail.com>
2024-10-27 18:12:00 -0500

    Added an `Adjustments` struct and moved starts and ends to an array of those. sorted the adjustments.

--- GPXEditor.py
+++ GPXEditor.py
@@ -2,7 +2,12 @@ import argparse
 import datetime
 import gpxpy
 import gpxpy.gpx
+from dataclasses import dataclass
 
+@dataclass
+class Adjustment:
+    start: datetime.datetime
+    end: datetime.datetime
 
 def convert_to_datetime(s):
     # add a UTC timezone offset to the end of the time
@@ -46,28 +51,28 @@ def create_arg_parser():
 # determine if the provided starts/ends are consistent:
 #   1. is each end later than it's corresponding start?
 #   2. If the timespans of the adjustments overlap, that is bad
-def are_adjustments_consistent(starts, ends):
+def are_adjustments_consistent(adjustments):
     # first check - make sure each end is after its start
-    for this_start, this_end in zip(starts, ends):
-        if this_end < this_start:
-            print("Provided adjustment start:", this_start, " occurs after corresponding adjustment end:", this_end)
+    for this_adjustment in adjustments:
+        if this_adjustment.end < this_adjustment.start:
+            print("Provided adjustment start:", this_adjustment.start, " occurs after corresponding adjustment end:", this_adjustment.end)
             return False
     # second check - make sure timespans don't overlap
     reference_index = 0
-    for reference_start, reference_end in zip(starts, ends):
+    for reference_adjustment in adjustments:
         comparison_index = 0
-        for comparison_start, comparison_end in zip(starts, ends):
+        for comparison_adjustment in adjustments:
             if reference_index != comparison_index: # don't want to compare a region to itself
-                if reference_start < comparison_start and reference_end > comparison_start:
+                if reference_adjustment.start < comparison_adjustment.start and reference_adjustment.end > comparison_adjustment.start:
                     # comparison_start is inside reference region
                     return False
-                if reference_start < comparison_end and reference_end > comparison_end:
+                if reference_adjustment.start < comparison_adjustment.end and reference_adjustment.end > comparison_adjustment.end:
                     # comparison_end is inside reference region
                     return False
-                if reference_start > comparison_start and reference_start < comparison_end:
+                if reference_adjustment.start > comparison_adjustment.start and reference_adjustment.start < comparison_adjustment.end:
                     # reference_start is inside comparison region
                     return False
-                if reference_end > comparison_start and reference_end < comparison_end:
+                if reference_adjustment.end > comparison_adjustment.start and reference_adjustment.end < comparison_adjustment.end:
                     # reference_end is inside comparison region
                     return False
             comparison_index += 1
@@ -75,20 +80,30 @@ def are_adjustments_consistent(starts, ends):
     # passed everything, must be good
     return True
 
+def sort_adjustments(starts, ends):
+    zipped = zip(starts, ends)
+    zipsorted = sorted(zipped)
+    sorted_adjustments = []
+    for this_start, this_end in zipsorted:
+        new_adjustment = Adjustment(this_start, this_end)
+        sorted_adjustments.extend((new_adjustment,))
+    return sorted_adjustments
+    
+
+
 def main():
     arg_parser = create_arg_parser()
 
     args = arg_parser.parse_args()
 
-    print(args.remove_before)
-    print('start:', args.adjustment_start)
-    print('end:', args.adjustment_end)
     if type(args.adjustment_start) != type(args.adjustment_end) or len(args.adjustment_start) != len(args.adjustment_end):
         print("Each 'adjustment-start' argument must be paired with an 'adjustment-end' argument")
         arg_parser.print_help()
         exit(1)
+    
+    adjustments = sort_adjustments(args.adjustment_start, args.adjustment_end)
 
-    if not are_adjustments_consistent(args.adjustment_start, args.adjustment_end):
+    if not are_adjustments_consistent(adjustments):
         print('Provided adjustment regions are not consistent. Either an end is before a start, or the adjustment regions overlap')
         arg_parser.print_help()
         exit(1)
@@ -109,14 +124,14 @@ def main():
                     segment.remove_point(0) # it's always zero, because we're removing from the beginning
                     current_point_index = current_point_index - 1
 
-                if len(args.adjustment_start) > 0:
-                    for this_start, this_end in zip(args.adjustment_start, args.adjustment_end):
-                        adjustment_delta = this_end - this_start
-                        if point.time > this_start and point.time < this_end:
+                if len(adjustments) > 0:
+                    for this_adjustment in adjustments:
+                        adjustment_delta = this_adjustment.end - this_adjustment.start
+                        if point.time > this_adjustment.start and point.time < this_adjustment.end:
                             print("Removing point at time ", point.time)
                             segment.remove_point(current_point_index)
                             current_point_index = current_point_index -1
-                        if point.time >= this_end:
+                        if point.time >= this_adjustment.end:
                             point.adjust_time(-adjustment_delta)
                 current_point_index = current_point_index + 1
 
