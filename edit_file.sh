3bcc4663366ca52395fda3c2308195184d7339b6
Derek D. Miller <dreemkiller@gmail.com>
2024-10-26 21:00:34 -0500

    Working, fairly modular. More than one adjustment region is untested

---
+++ edit_file.sh
@@ -0,0 +1,5 @@
+python3 GPXEditor.py \
+    --remove-before '2024-10-18 13:15:57' \
+    --adjustment-start '2024-10-18 14:39:11' \
+    --adjustment-end '2024-10-18 14:40:48' \
+    --input ../2024.10.18\ SATN\ Social\ North/SATN_Social_North.gpx --output ./crap.gpx
